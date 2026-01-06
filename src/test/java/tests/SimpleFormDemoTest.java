c16f94584496c85899f7e85478a3cae2e6f736f2
Nirojakatroth <133659190+Nirojakatroth@users.noreply.github.com>
2026-01-06 17:07:42 +0530

    Update SimpleFormDemoTest.java

--- src/test/java/tests/SimpleFormDemoTest.java
+++ src/test/java/tests/SimpleFormDemoTest.java
@@ -1,79 +1,3 @@
-/*
-package tests;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-import pages.SimpleFormDemoPage;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.Duration;
-import java.util.HashMap;
-
-public class SimpleFormDemoTest {
-    protected WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() throws MalformedURLException {
-        String username = "katrothniroja";
-        String accessKey = "LT_xxjDonidZuE4ILQ6qDd3VzcBihyoOd846Hio6zINMI0Gzwv";
-
-
-        String gridURL = "https://" + username + ":" + accessKey + "@hub.lambdatest.com/wd/hub";
-
-        // Desired Capabilities for Firefox
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", "Firefox");
-        capabilities.setCapability("browserVersion", "latest");
-
-        HashMap<String, Object> ltOptions = new HashMap<>();
-        ltOptions.put("platformName", "Windows 10");
-        ltOptions.put("project", "Selenium 101 Certification");
-        ltOptions.put("build", "Simple Form Demo Test");
-        ltOptions.put("name", "Simple Form Demo - Firefox Test");
-        ltOptions.put("selenium_version", "4.0.0");
-
-        // Enable logs
-        ltOptions.put("network", true);
-        ltOptions.put("visual", true);
-        ltOptions.put("video", true);
-        ltOptions.put("console", true);
-
-        capabilities.setCapability("LT:Options", ltOptions);
-
-        driver = new RemoteWebDriver(new URL(gridURL), capabilities);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.lambdatest.com/selenium-playground");
-    }
-
-    @Test
-    public void testSimpleFormDemo() {
-
-        try {
-
-            driver.get("https://www.lambdatest.com/selenium-playground/simple-form-demo");
-
-            Assert.assertTrue(driver.getCurrentUrl().contains("simple-form-demo"), "URL does not contain 'simple-form-demo'.");
-
-            SimpleFormDemoPage page = new SimpleFormDemoPage(driver);
-
-            String message = "Welcome to LambdaTest";
-            page.enterMessage(message);
-            page.clickShowMessage();
-
-            String outputMessage = page.getOutputMessage();
-            Assert.assertEquals(outputMessage, message, "Message does not match!");
-        } catch (Exception e) {
-            throw e;
-        }
-    }
-}
-*/
-
 package tests;
 
 import org.openqa.selenium.WebDriver;
@@ -159,4 +83,5 @@ public class SimpleFormDemoTest {
         }
     }
 }
+
 
